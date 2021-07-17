7129c0679a022562c5c52788f93608ab9c769eb5
Alexsandrab <sandrinhaleto@hotmail.com>
2021-07-17 12:33:17 -0300

    primeiro commit

---
+++ descobrirValor.js
@@ -0,0 +1,53 @@
+var erros = [];
+var numSorteado = Math.floor(Math.random()*100)+1;
+const chances = 6;
+function apostar(){
+    var Innum = document.getElementById("num");
+    var num = Number(Innum.value);
+    var respChances = document.getElementById("chances");
+    var respErros = document.getElementById("erros");
+    var dica = document.getElementById("dica");
+
+    if (num<=0 || num>100 || isNaN(num)){
+        alert("Digite um valor válido que seja acima de 0 e menor que 100!")
+        Innum.value = "";
+        Innum.focus();
+        return;
+    }
+    if (num == numSorteado){
+        alert("Parabéns!! Você acertou o Número!")
+        dica.textContent = "Muito bem! , o valor sorteado foi " + numSorteado;
+    }
+    if (erros.indexOf(num) >=0 ){
+        alert("Valor já digitado!")
+        Innum.value = "";
+        Innum.focus();
+    }else{
+        erros.push(num);
+    }
+   
+    for (var i = 1; i<=erros.length;i++){
+        var numChances = i;
+        var totalchances = chances - numChances;
+        respChances.textContent = chances - numChances;
+        respErros.textContent = erros.length + "("+ erros.join(",")+")";
+    }
+    if (totalchances == 0){
+        alert("Chances acabaram!!")
+        dica.textContent = "Você não acertou o número, o valor sorteado foi " + numSorteado + ".";
+        return
+    }else{
+        var respDica = num < numSorteado? "maior" : "menor";
+        dica.textContent = "Digite um valor " + respDica + " que " + num;
+    }
+    Innum.value = "";
+    Innum.focus();
+}
+var aposta = document.getElementById("aposta");
+aposta.addEventListener("click", apostar);
+
+function jogarNovamente(){
+    location.reload();
+}
+var jogarDenovo = document.getElementById("jogarDenovo");
+jogarDenovo.addEventListener("click", jogarNovamente);
